28b3d3bbddb5300ff39d06f7ee0d04f36f60e2f6
sapph42 <nick.gibson@gmail.com>
2023-10-15 15:36:43 -0400

    Updated to write config file

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Data.SqlClient;
-using System.IO;
-using System.Reflection;
 using System.Windows.Forms;
 using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
 using SapphTools.Utils.UX;
 using Tulpep.ActiveDirectoryObjectPicker;
+using System.Xml;
+using System.Linq;
 
 namespace SapphTools.LogLookup.Setup {
     internal static class Program {
@@ -19,12 +19,9 @@ namespace SapphTools.LogLookup.Setup {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            string LDAPRoot = "";
-            string LDAPWorkstation = "";
-            string allowedGroups;
-            string OUGroups;
-            string SiteCode;
-            string SQLServer;
+            string configPath = "";
+            Dictionary<string, string> configValues = new Dictionary<string, string>();
+            XmlDocument xmlSettings = new XmlDocument();
             TextEntryMessageBox msgBox;
 
             TextEntryMessageBoxIcon TextIcon = TextEntryMessageBoxIcon.Question;
@@ -32,15 +29,15 @@ namespace SapphTools.LogLookup.Setup {
             System.Drawing.Font TextFont = new System.Drawing.Font("Tahoma", 11);
             MessageBoxButtons TextButtons = MessageBoxButtons.OKCancel;
 
-            //msgBox = TextEntryMessageBoxManager.CreateMessageBox("Site Code");
-            //msgBox.Caption = "Site Code";
-            //msgBox.Text = "What is your site code?";
-            //msgBox.Icon = TextIcon;
-            //msgBox.InputMaxLength = 6;
-            //msgBox.Font = TextFont;
-            //msgBox.AddButtons(TextButtons);
+            msgBox = TextEntryMessageBoxManager.CreateMessageBox("Site Code");
+            msgBox.Caption = "Site Code";
+            msgBox.Text = "What is your site code?";
+            msgBox.Icon = TextIcon;
+            msgBox.InputMaxLength = 6;
+            msgBox.Font = TextFont;
+            msgBox.AddButtons(TextButtons);
 
-            //SiteCode = msgBox.Show().ToUpper();
+            configValues.Add("SiteCode", msgBox.Show().ToUpper());
 
             msgBox = TextEntryMessageBoxManager.CreateMessageBox("SQL Server FQDN");
             msgBox.Caption = "SQL Server FQDN";
@@ -51,61 +48,84 @@ namespace SapphTools.LogLookup.Setup {
             msgBox.Font = TextFont;
             msgBox.AddButtons(TextButtons);
 
-            SQLServer = msgBox.Show();
+            configValues.Add("SQLServer", msgBox.Show().ToUpper());
+            configValues.Add("Database", "EUDLogging");
+            configValues.Add("ConnectionString", $@"Data Source={configValues["SQLServer"]};Initial Catalog={configValues["Database"]};Authentication=Active Directory Integrated");
 
-            //using (OUPicker form = new OUPicker("Select the root OU for your site.")) {
-            //    DialogResult result = form.ShowDialog();
-            //    if (result == DialogResult.OK) {
-            //        LDAPRoot = form.SelectedOU;
-            //    } else
-            //        Application.Exit();
-            //}
-            //using (OUPicker form = new OUPicker("Select the root OU for your site's workstations.")) {
-            //    DialogResult result = form.ShowDialog();
-            //    if (result == DialogResult.OK) {
-            //        LDAPRoot = form.SelectedOU;
-            //    } else
-            //        Application.Exit();
-            //}
-            //MessageBox.Show("In the next dialog, select the Security Groups permitted to launch this application.");
-            //DirectoryObjectPickerDialog picker = new DirectoryObjectPickerDialog() {
-            //    AllowedObjectTypes = ObjectTypes.Groups,
-            //    DefaultObjectTypes = ObjectTypes.Computers,
-            //    AllowedLocations = Locations.All,
-            //    DefaultLocations = Locations.JoinedDomain,
-            //    MultiSelect = true,
-            //    ShowAdvancedView = true
-            //};
-            //using (picker) {
-            //    List<string> results = new List<string>();
-            //    if (picker.ShowDialog() == DialogResult.OK) {
-            //        foreach (var sel in picker.SelectedObjects) {
-            //            results.Add(sel.Name);
-            //        }
-            //    }
-            //    allowedGroups = String.Join(",", results.ToArray());
-            //}
-            //MessageBox.Show("In the next dialog, select the Security Groups permitted enable and disable objects.");
-            //picker = new DirectoryObjectPickerDialog() {
-            //    AllowedObjectTypes = ObjectTypes.Groups,
-            //    DefaultObjectTypes = ObjectTypes.Computers,
-            //    AllowedLocations = Locations.All,
-            //    DefaultLocations = Locations.JoinedDomain,
-            //    MultiSelect = true,
-            //    ShowAdvancedView = true
-            //};
-            //using (picker) {
-            //    List<string> results = new List<string>();
-            //    if (picker.ShowDialog() == DialogResult.OK) {
-            //        foreach (var sel in picker.SelectedObjects) {
-            //            results.Add(sel.Name);
-            //        }
-            //    }
-            //    OUGroups = String.Join(",", results.ToArray());
-            //}
+            using (OUPicker form = new OUPicker("Select the root OU for your site.")) {
+                DialogResult result = form.ShowDialog();
+                if (result == DialogResult.OK) {
+                    configValues.Add("LDAPRoot", form.SelectedOU);
+                } else
+                    Application.Exit();
+            }
+            using (OUPicker form = new OUPicker("Select the root OU for your site's workstations.")) {
+                DialogResult result = form.ShowDialog();
+                if (result == DialogResult.OK) {
+                    configValues.Add("LDAPWorkstation", form.SelectedOU);
+                } else
+                    Application.Exit();
+            }
+            MessageBox.Show("In the next dialog, select the Security Groups permitted to launch this application.");
+            DirectoryObjectPickerDialog picker = new DirectoryObjectPickerDialog() {
+                AllowedObjectTypes = ObjectTypes.Groups,
+                DefaultObjectTypes = ObjectTypes.Computers,
+                AllowedLocations = Locations.All,
+                DefaultLocations = Locations.JoinedDomain,
+                MultiSelect = true,
+                ShowAdvancedView = true
+            };
+            using (picker) {
+                List<string> results = new List<string>();
+                if (picker.ShowDialog() == DialogResult.OK) {
+                    foreach (var sel in picker.SelectedObjects) {
+                        results.Add(sel.Name);
+                    }
+                }
+                configValues.Add("allowedGroups", String.Join(",", results.ToArray()));
+            }
+            MessageBox.Show("In the next dialog, select the Security Groups permitted enable and disable objects.");
+            picker = new DirectoryObjectPickerDialog() {
+                AllowedObjectTypes = ObjectTypes.Groups,
+                DefaultObjectTypes = ObjectTypes.Computers,
+                AllowedLocations = Locations.All,
+                DefaultLocations = Locations.JoinedDomain,
+                MultiSelect = true,
+                ShowAdvancedView = true
+            };
+            using (picker) {
+                List<string> results = new List<string>();
+                if (picker.ShowDialog() == DialogResult.OK) {
+                    foreach (var sel in picker.SelectedObjects) {
+                        results.Add(sel.Name);
+                    }
+                }
+                configValues.Add("OUGroups", String.Join(",", results.ToArray()));
+            }
+
+            configValues.Add("Initialized", "True");
+            OpenFileDialog settingsDialog = new OpenFileDialog() {
+                CheckFileExists = true,
+                Multiselect = false,
+                Title = "Location for LogLookupTool.exe.config",
+                Filter = "config files (*.config)|*.config"
+            };
+            if (settingsDialog.ShowDialog() == DialogResult.OK) {
+                configPath = settingsDialog.FileName;
+            } else {
+                Application.Exit();
+            }
+            xmlSettings.Load(configPath);
+            XmlNodeList settingsNodes = xmlSettings.SelectNodes(@"/configuration/applicationSettings/Log_Lookup_Tool.Properties.Settings");
+            foreach (XmlNode settingsNode in settingsNodes[0].ChildNodes) {
+                if (configValues.Keys.Contains(settingsNode.Attributes["name"].Value)) {
+                    settingsNode.FirstChild.InnerText = configValues[settingsNode.Attributes["name"].Value];
+                }
+            }
+            xmlSettings.Save(configPath);
 
             SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder() {
-                DataSource = SQLServer,
+                DataSource = configValues["SQLServer"],
                 InitialCatalog = "master",
                 IntegratedSecurity = true,
                 TrustServerCertificate = true
@@ -123,7 +143,7 @@ namespace SapphTools.LogLookup.Setup {
                     object result = cmd.ExecuteScalar();
                     if (result == DBNull.Value || (int)result != 1) {
                         MessageBox.Show(
-                            $"This user does not have enough permissions to create a database on {SQLServer}.",
+                            $"This user does not have enough permissions to create a database on {configValues["SQLServer"]}.",
                             "Access denied.",
                             MessageBoxButtons.OK,
                             MessageBoxIcon.Stop
